451a1d77b9925268744b667ac9b12ecd02fa3bc4
haowen112 <liuhaowen60@gmail.com>
2019-12-11 00:00:03 -0500

    First version done. Needs to add css

--- groups/views.py
+++ groups/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.urls import reverse
 from django.views import generic
 from groups.models import Group, GroupMember
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from . import models
 # Create your views here.
 
 
@@ -17,3 +20,41 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except:
+            messages.warning(self.request, ('Warning already a member'))
+        else:
+            messages.warning(self.request, ('You are now a member'))
+
+        return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            membership = models.GroupMember.objects.filter(
+                user = self.request.user,
+                group__slug = self.kwargs.get('slug')
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, ('You are not in this group'))
+        else:
+            membership.delete()
+            messages.warning(self.request, ('You have left the group'))
+
+        return super().get(request, *args, **kwargs)
